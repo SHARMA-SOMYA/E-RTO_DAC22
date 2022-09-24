245c5329deb2bfb115e5ad0862734872933740eb
Somya Sharma <contactsomya26@gmail.com>
2022-09-24 11:15:44 +0530

    project commit - somya

---
+++ Erto/eRto1/src/main/java/com/app/dto/MessageDto.java
@@ -0,0 +1,18 @@
+package com.app.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class MessageDto {
+
+	
+	private String m1;
+	private String m2;
+	private String m3;
+	private String m4;
+	
+}
